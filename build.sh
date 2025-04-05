86c90933a19f08bd77f64d5d7a89e89873cb21e2
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-05 19:26:47 +0800

    Update build.sh, add git config for user identity.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 pip install -r requirements.txt
+git config --global user.name "HananiahKao"
+git config --global user.email "HananiahKao@users.noreply.github.com"
 ./fetch_github_repos.sh &
 python3 ./train-and-generate.py &
 echo started training...
