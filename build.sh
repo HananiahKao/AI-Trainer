af3098f833c97350e8c6fa00ad43c194b9fc1d6c
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-05 20:45:14 +0800

    Update build.sh, direct stderr into stdout

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 pip install -r requirements.txt
 git config --global user.name "HananiahKao"
 git config --global user.email "HananiahKao@users.noreply.github.com"
-./fetch_github_repos.sh > fetch_repo_status.html &
-python3 ./train-and-generate.py > training_status.html &
+./fetch_github_repos.sh 2>&1 > fetch_repo_status.html &
+python3 ./train-and-generate.py 2>&1 > training_status.html &
 echo started training...
