fbe6f1befdea1a72e4c203ddc9e6b2040e3d3755
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-05 19:50:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 pip install -r requirements.txt
 git config --global user.name "HananiahKao"
 git config --global user.email "HananiahKao@users.noreply.github.com"
-./fetch_github_repos.sh &
-python3 ./train-and-generate.py &
+./fetch_github_repos.sh > fetch_repo_status &
+python3 ./train-and-generate.py > training_status &
 echo started training...
