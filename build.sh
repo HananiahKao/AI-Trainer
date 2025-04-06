3d00c035bcfdd4beca7110c2a8f41f7da348ff0b
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-06 19:39:38 +0800

    Update build.sh, remove the lines with git config.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 pip install -r requirements.txt
-git config --global user.name "HananiahKao"
-git config --global user.email "HananiahKao@users.noreply.github.com"
 ./fetch_github_repos.sh > fetch_repo_status.html 2>&1 &
 python3 ./train-and-generate.py > training_status.html 2>&1 &
 echo started training...
