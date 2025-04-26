33a32ee125bcaac994eff0b0358e0c8fe2de4599
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-26 10:37:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-pip install -r requirements.txt
-python3 ./train-and-generate.py > training_status.html 2>&1 &
-echo started training...
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull mistral
