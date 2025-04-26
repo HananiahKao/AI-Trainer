6a66e49865acc8d65091dff84b8b1f26a51e3339
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-27 07:49:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ git clone https://github.com/Homebrew/brew homebrew
 eval "$(homebrew/bin/brew shellenv)"
 brew update --force
 chmod -R go-w "$(brew --prefix)/share/zsh"
-brew install --cask ollama
+brew install ollama
+ollama serve &
 ollama pull mistral
 pip3 install -r requirements.txt
